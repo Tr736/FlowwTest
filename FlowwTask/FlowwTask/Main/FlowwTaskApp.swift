414d62d5cefd9fd852f88cfa8d29b8326c5d91de
Thomas <tr736@hotmail.com>
2023-02-02 10:42:52 +0000

    init project

---
+++ FlowwTask/FlowwTask/Main/FlowwTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowwTaskApp.swift
+//  FlowwTask
+//
+//  Created by Thomas Richardson on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowwTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
